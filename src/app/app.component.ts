10c63a78b028bbd3ac98c91cb933e4cf3949cf9b
Adjectival <alexd.jacks@gmail.com>
2017-09-02 12:36:20 -0700

    :sparkles: adds mock data for army

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,64 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Momo',
+    property: 'rock',
+    type: 'Attack',
+  },
+  {
+    id: 2,
+    name: 'Mu',
+    property: 'paper',
+    type: 'Balance',
+  },
+  {
+    id: 3,
+    name: 'Carrot',
+    property: 'paper',
+    type: 'Attack',
+  },
+  {
+    id: 4,
+    name: 'Deborah',
+    property: 'scissors',
+    type: 'AreaAttack',
+  },
+  {
+    id: 5,
+    name: 'Kai',
+    property: 'rock',
+    type: 'Balance',
+  },
+  {
+    id: 6,
+    name: 'Alfred',
+    property: 'rock',
+    type: 'AreaAttack',
+  },
+  {
+    id: 7,
+    name: 'Lance',
+    property: 'scissors',
+    type: 'Defense',
+  },
+  {
+    id: 8,
+    name: 'Mas',
+    property: 'paper',
+    type: 'Support',
+  },
+  {
+    id: 9,
+    name: 'Ian',
+    property: 'rock',
+    type: 'Balance',
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +67,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title1 = 'Fantasy War Tactics';
   title2 = 'army sorting app';
+  heroes = HEROES;
+
 }
