03c130b6fe3f76d7b95b99278fa39c272f2c49fc
Adjectival <alexd.jacks@gmail.com>
2017-09-02 10:37:48 -0700

    :sparkles: splits dropdown into 2 dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fwtparty';
+  title1 = 'Fantasy War Tactics';
+  title2 = 'army sorting app';
 }
