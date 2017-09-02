144e3be7efdedf4e73df0d07279031517cf1c139
Adjectival <alexd.jacks@gmail.com>
2017-09-02 15:14:19 -0700

    :wrench: refactors model and data out of main component, adds more hero data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,8 @@
 import { Component } from '@angular/core';
 
 import { Hero } from './hero';
+import { HEROES } from './herodata';
 
-const HEROES: Hero[] = [
-  {
-    id: 1,
-    name: 'Momo',
-    property: 'rock',
-    type: 'Attack',
-  },
-  {
-    id: 2,
-    name: 'Mu',
-    property: 'paper',
-    type: 'Balance',
-  },
-  {
-    id: 3,
-    name: 'Carrot',
-    property: 'paper',
-    type: 'Attack',
-  },
-  {
-    id: 4,
-    name: 'Deborah',
-    property: 'scissors',
-    type: 'AreaAttack',
-  },
-  {
-    id: 5,
-    name: 'Kai',
-    property: 'rock',
-    type: 'Balance',
-  },
-  {
-    id: 6,
-    name: 'Alfred',
-    property: 'rock',
-    type: 'AreaAttack',
-  },
-  {
-    id: 7,
-    name: 'Lance',
-    property: 'scissors',
-    type: 'Defense',
-  },
-  {
-    id: 8,
-    name: 'Mas',
-    property: 'paper',
-    type: 'Support',
-  },
-  {
-    id: 9,
-    name: 'Ian',
-    property: 'rock',
-    type: 'Balance',
-  }
-];
 
 @Component({
   selector: 'app-root',
